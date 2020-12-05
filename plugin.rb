bccaee088b01a55c0b75ec0c5198df99b2045187
Tim Bass <neo@unix.com>
2020-12-05 19:30:05 +0700

    REFACTOR:  With File.expand_path()

--- plugin.rb
+++ plugin.rb
@@ -2,22 +2,22 @@
 
 # name: discourse-selfish-seo-layout-changes
 # about: selfish plugin to remove canonical url, generator and change powered by, etc.
-# version: 0.0.32
+# version: 0.0.33
 # date: 5 December 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-selfish-seo-layout-changes
 
 PLUGIN_NAME = "discourse-selfish-seo-layout-changes"
-APP_ROOT = "#{Rails.root}/plugins/#{PLUGIN_NAME}/app"
-PLUGIN_LOGIC = "#{APP_ROOT}/lib/selfish_layout_changes.rb"
-load File.open(PLUGIN_LOGIC)
-
-MONKEY_PATCH = "#{APP_ROOT}/lib/monkey_patch_topics_controller.rb"
+#APP_ROOT = "#{Rails.root}/plugins/#{PLUGIN_NAME}/app"
+#PLUGIN_LOGIC = "#{APP_ROOT}/lib/selfish_layout_changes.rb"
+#load File.open(PLUGIN_LOGIC)
+load File.expand_path("app/lib/selfish_layout_changes.rb", __FILE__)
+#MONKEY_PATCH = "#{APP_ROOT}/lib/monkey_patch_topics_controller.rb"
 
 after_initialize do
   SelfishSeoLayoutChanges.modify_head_layout
   SelfishSeoLayoutChanges.modify_crawler_layout
   SelfishSeoLayoutChanges.modify_application_layout
 
-  load File.open(MONKEY_PATCH)
+  load File.open("app/lib/monkey_patch_topics_controller.rb", __FILE_)
 end
