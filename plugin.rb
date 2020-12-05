25a540912559ea81379db977fb80c5d1653345ae
Tim Bass <neo@unix.com>
2020-12-05 12:42:45 +0700

    REFACTOR: class_eval to instance_eval

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-selfish-seo-layout-changes
 # about: selfish plugin to remove canonical url, generator and change powered by, etc.
-# version: 0.0.26
+# version: 0.0.27
 # date: 5 December 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-selfish-seo-layout-changes
@@ -17,7 +17,7 @@ after_initialize do
   SelfishSeoLayoutChanges.modify_crawler_layout
   SelfishSeoLayoutChanges.modify_application_layout
 
-  TopicsController.class_eval do
+  TopicsController.instance_eval do
     after_action :strip_newlines_and_more_from_meta_description
 
     private
