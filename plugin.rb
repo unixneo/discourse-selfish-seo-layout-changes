ef4daf2785136738c2bb60d5d8afc28e9532a829
Tim Bass <neo@unix.com>
2020-12-05 15:27:21 +0700

    DEV:  Change to class overwrite.

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-selfish-seo-layout-changes
 # about: selfish plugin to remove canonical url, generator and change powered by, etc.
-# version: 0.0.297
+# version: 0.0.298
 # date: 5 December 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-selfish-seo-layout-changes
@@ -17,20 +17,38 @@ after_initialize do
   SelfishSeoLayoutChanges.modify_crawler_layout
   SelfishSeoLayoutChanges.modify_application_layout
 
-  TopicsController.class_eval do
-    before_action :strip_newlines_and_more_from_meta_description, only: [:perform_show_response]
+  # overwrite TopicsController private method to clean up @description_meta text
+  class TopicsController
+    def perform_show_response
+      if request.head?
+        head :ok
+        return
+      end
 
-    private
+      topic_view_serializer = TopicViewSerializer.new(
+        @topic_view,
+        scope: guardian,
+        root: false,
+        include_raw: !!params[:include_raw],
+      )
 
-    def strip_newlines_and_more_from_meta_description
-      Discourse.warn(">>>>NEONEO_BEGIN>>>>> @topic_view.summary: #{@topic_view&.summary}", uri: request.env["REQUEST_URI"])
-      tmp_description = @topic_view&.summary
-      tmp_description = tmp_description&.gsub("&amp;hellip;", "")
-      tmp_description = tmp_description&.gsub("&hellip;", "")
-      tmp_description = tmp_description&.gsub(/\n+/, " ")
-      tmp_description = tmp_description&.gsub(/\s+/, " ")&.strip
-      #@topic_view.summary = tmp_description
-      Discourse.warn(">>>>NEONEO_END>>>>> tmp_description: #{tmp_description}", uri: request.env["REQUEST_URI"])
+      respond_to do |format|
+        format.html do
+          @tags = SiteSetting.tagging_enabled ? @topic_view.topic.tags : []
+          @breadcrumbs = helpers.categories_breadcrumb(@topic_view.topic) || []
+          @description_meta = @topic_view.topic.excerpt.present? ? @topic_view.topic.excerpt : @topic_view.summary
+          @description_meta = @description_meta&.gsub("&amp;hellip;", "")
+          @description_meta = @description_meta&.gsub("&hellip;", "")
+          @description_meta = @description_meta&.gsub(/\n+/, " ")
+          @description_meta = @description_meta&.gsub(/\s+/, " ")&.strip
+          store_preloaded("topic_#{@topic_view.topic.id}", MultiJson.dump(topic_view_serializer))
+          render :show
+        end
+
+        format.json do
+          render_json_dump(topic_view_serializer)
+        end
+      end
     end
   end
 end
