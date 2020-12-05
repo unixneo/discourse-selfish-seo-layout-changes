12b3318ec892891155187c5e2b60841cb6089359
Tim Bass <neo@unix.com>
2020-12-05 13:06:45 +0700

    BUMP: version

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-selfish-seo-layout-changes
 # about: selfish plugin to remove canonical url, generator and change powered by, etc.
-# version: 0.0.27
+# version: 0.0.28
 # date: 5 December 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-selfish-seo-layout-changes
