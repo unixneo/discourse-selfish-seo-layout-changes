0fca7c450bade4459983901996be0cc409f0ba4f
Tim Bass <neo@unix.com>
2020-12-05 16:11:17 +0700

    REVERT:   Added line of code back in.

--- app/lib/monkey_patch_topics_controller.rb
+++ app/lib/monkey_patch_topics_controller.rb
@@ -16,6 +16,7 @@ TopicsController.class_eval do
       format.html do
         @tags = SiteSetting.tagging_enabled ? @topic_view.topic.tags : []
         @breadcrumbs = helpers.categories_breadcrumb(@topic_view.topic) || []
+        @description_meta = @topic_view.topic.excerpt.present? ? @topic_view.topic.excerpt : @topic_view.summary
         @description_meta = @description_meta&.gsub("&amp;hellip;", " ")
         @description_meta = @description_meta&.gsub("&hellip;", " ")
         @description_meta = @description_meta&.gsub(/\n+/, " ")
