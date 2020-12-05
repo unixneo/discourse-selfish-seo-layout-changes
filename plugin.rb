f7d3d0570404815248246427d85d208fddc7bbbb
Tim Bass <neo@unix.com>
2020-12-05 13:46:43 +0700

    DEBUG:  Add Discourse.warn for debugging.

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-selfish-seo-layout-changes
 # about: selfish plugin to remove canonical url, generator and change powered by, etc.
-# version: 0.0.29
+# version: 0.0.291
 # date: 5 December 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-selfish-seo-layout-changes
@@ -23,6 +23,7 @@ after_initialize do
     private
 
     def strip_newlines_and_more_from_meta_description
+      Discourse.warn(">>>>NEONEO>>>>> @description_meta; #{@description_meta}", uri: request.env["REQUEST_URI"])
       @description_meta&.gsub("&amp;hellip;", "")
       @description_meta&.gsub(/\n+/, " ")
       @description_meta&.gsub(/\s+/, " ")&.strip
