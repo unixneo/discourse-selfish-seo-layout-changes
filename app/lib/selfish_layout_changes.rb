3c128f25e34b935c327dcbbf7a5fad493e1aed68
Tim Bass <neo@unix.com>
2020-12-05 10:47:14 +0700

    REFACTOR: Add newline to inserted line(s).

--- app/lib/selfish_layout_changes.rb
+++ app/lib/selfish_layout_changes.rb
@@ -2,7 +2,7 @@ class SelfishSeoLayoutChanges
   def self.modify_head_layout
     tmp_file = "/shared/tmp/head_work.tmp.txt"
     head_layout = "#{Rails.root}/app/views/layouts/_head.html.erb"
-    google_site_verification = '<meta name="google-site-verification" content="IRSOCxclhQ3ynQHh5zO2js5hftZ4UYTrk_iImCo5sIg" />\n'
+    google_site_verification = '<meta name="google-site-verification" content="IRSOCxclhQ3ynQHh5zO2js5hftZ4UYTrk_iImCo5sIg" />' + "\n"
     if File.readlines(head_layout).grep(/canonical/)&.any?
       IO.write(tmp_file, google_site_verification)
       IO.foreach(head_layout) do |line|
@@ -16,7 +16,7 @@ class SelfishSeoLayoutChanges
     tmp_file = "/shared/tmp/crawler_work.tmp.txt"
     crawler_layout = "#{Rails.root}/app/views/layouts/crawler.html.erb"
     if File.readlines(crawler_layout).grep(/www\.unix\.com/)&.empty?
-      powered_by_link = '<p class="powered-by-link">Powered by <a href="https://www.unix.com/">UNIX.com</a>, best viewed with JavaScript enabled.</p>'
+      powered_by_link = '<p class="powered-by-link">Powered by <a href="https://www.unix.com/">UNIX.com</a>, best viewed with JavaScript enabled.</p>' + "\n"
       tmp_file = "/shared/tmp/crawler_work.tmp.txt"
 
       IO.foreach(crawler_layout) do |line|
@@ -29,10 +29,12 @@ class SelfishSeoLayoutChanges
       FileUtils.mv(tmp_file, crawler_layout)
     end
   end
+
+  # This method is not necessary; but just in case :)
   def self.modify_application_layout
     application_layout = "#{Rails.root}/app/views/layouts/crawler.html.erb"
     if File.readlines(application_layout).grep(/www\.unix\.com/)&.empty?
-      powered_by_link = '<p class="powered-by-link">Powered by <a href="https://www.unix.com/">UNIX.com</a>, best viewed with JavaScript enabled.</p>'
+      powered_by_link = '<p class="powered-by-link">Powered by <a href="https://www.unix.com/">UNIX.com</a>, best viewed with JavaScript enabled.</p>' + "\n"
       tmp_file = "/shared/tmp/application_work.tmp.txt"
 
       IO.foreach(application_layout) do |line|
--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-selfish-seo-layout-changes
 # about: selfish plugin to remove canonical url, generator and change powered by, etc.
-# version: 0.0.21
+# version: 0.0.22
 # date: 5 December 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-selfish-seo-layout-changes
