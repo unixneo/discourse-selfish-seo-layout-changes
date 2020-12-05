2ae4c06591b6ccbcf11b2f1b4c5639150dcdc10d
Tim Bass <neo@unix.com>
2020-12-05 10:26:02 +0700

    REFACTOR:  Move google site verification to head.

--- app/lib/selfish_layout_changes.rb
+++ app/lib/selfish_layout_changes.rb
@@ -1,9 +1,9 @@
 class SelfishSeoLayoutChanges
   def self.modify_head_layout
     head_layout = "#{Rails.root}/app/views/layouts/_head.html.erb"
+    google_site_verification = '<meta name="google-site-verification" content="IRSOCxclhQ3ynQHh5zO2js5hftZ4UYTrk_iImCo5sIg" />\n'
     if File.readlines(head_layout).grep(/canonical/)&.any?
-      tmp_file = "/shared/tmp/head_work.tmp.txt"
-      IO.write(tmp_file, "")
+      IO.write(tmp_file, "google_site_verification")
       IO.foreach(head_layout) do |line|
         IO.write(tmp_file, line, mode: "a") unless ((line.include? "canonical") || (line.include? "generator"))
       end
@@ -24,15 +24,6 @@ class SelfishSeoLayoutChanges
         else
           IO.write(tmp_file, line, mode: "a")
         end
-      end
-      FileUtils.mv(tmp_file, crawler_layout)
-    end
-
-    if File.readlines(crawler_layout).grep(/google\-site\-verification/)&.empty?
-      google_site_verification = '<meta name="google-site-verification" content="IRSOCxclhQ3ynQHh5zO2js5hftZ4UYTrk_iImCo5sIg" />\n'
-      IO.write(tmp_file, google_site_verification)
-      IO.foreach(crawler_layout) do |line|
-        IO.write(tmp_file, line, mode: "a")
       end
       FileUtils.mv(tmp_file, crawler_layout)
     end
