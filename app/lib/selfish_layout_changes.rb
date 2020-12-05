3d6baee818c47d067e65a36b4caabf899d25566b
Tim Bass <neo@unix.com>
2020-12-05 10:38:42 +0700

    FIX: Typo.

--- app/lib/selfish_layout_changes.rb
+++ app/lib/selfish_layout_changes.rb
@@ -4,7 +4,7 @@ class SelfishSeoLayoutChanges
     head_layout = "#{Rails.root}/app/views/layouts/_head.html.erb"
     google_site_verification = '<meta name="google-site-verification" content="IRSOCxclhQ3ynQHh5zO2js5hftZ4UYTrk_iImCo5sIg" />\n'
     if File.readlines(head_layout).grep(/canonical/)&.any?
-      IO.write(tmp_file, "google_site_verification")
+      IO.write(tmp_file, google_site_verification)
       IO.foreach(head_layout) do |line|
         IO.write(tmp_file, line, mode: "a") unless ((line.include? "canonical") || (line.include? "generator"))
       end
@@ -16,7 +16,7 @@ class SelfishSeoLayoutChanges
     tmp_file = "/shared/tmp/crawler_work.tmp.txt"
     crawler_layout = "#{Rails.root}/app/views/layouts/crawler.html.erb"
     if File.readlines(crawler_layout).grep(/www\.unix\.com/)&.empty?
-      powered_by_link = '<p class="powered-by-link">Powered by <a href="https://www.unix.com/">UNIX.com</a>, best viewed with JavaScript enabled.</p>\n'
+      powered_by_link = '<p class="powered-by-link">Powered by <a href="https://www.unix.com/">UNIX.com</a>, best viewed with JavaScript enabled.</p>'
       tmp_file = "/shared/tmp/crawler_work.tmp.txt"
 
       IO.foreach(crawler_layout) do |line|
@@ -32,7 +32,7 @@ class SelfishSeoLayoutChanges
   def self.modify_application_layout
     application_layout = "#{Rails.root}/app/views/layouts/crawler.html.erb"
     if File.readlines(application_layout).grep(/www\.unix\.com/)&.empty?
-      powered_by_link = '<p class="powered-by-link">Powered by <a href="https://www.unix.com/">UNIX.com</a>, best viewed with JavaScript enabled.</p>\n'
+      powered_by_link = '<p class="powered-by-link">Powered by <a href="https://www.unix.com/">UNIX.com</a>, best viewed with JavaScript enabled.</p>'
       tmp_file = "/shared/tmp/application_work.tmp.txt"
 
       IO.foreach(application_layout) do |line|
