690febc02579a9a5c8b6fd6ecf278e223a9d5df6
Tim Bass <neo@unix.com>
2020-12-05 20:23:31 +0700

    REFACTOR: require to require_relative

--- plugin.rb
+++ plugin.rb
@@ -2,18 +2,18 @@
 
 # name: discourse-selfish-seo-layout-changes
 # about: selfish plugin to remove canonical url, generator and change powered by, etc.
-# version: 0.0.35
+# version: 0.0.36
 # date: 5 December 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-selfish-seo-layout-changes
 
 PLUGIN_NAME = "discourse-selfish-seo-layout-changes"
-require File.expand_path("../app/lib/selfish_layout_changes.rb", __FILE__)
+require_relative "./app/lib/selfish_layout_changes"
 
 after_initialize do
   SelfishSeoLayoutChanges.modify_head_layout
   SelfishSeoLayoutChanges.modify_crawler_layout
   SelfishSeoLayoutChanges.modify_application_layout
 
-  require File.expand_path("../app/lib/monkey_patch_topics_controller.rb", __FILE__)
+  require_relative "./app/lib/monkey_patch_topics_controller"
 end
