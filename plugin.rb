96aa3a2a06f277049bff677681f7e907555ee0df
Tim Bass <neo@unix.com>
2020-12-05 14:01:54 +0700

    REFACTOR: Move Debug line.

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-selfish-seo-layout-changes
 # about: selfish plugin to remove canonical url, generator and change powered by, etc.
-# version: 0.0.291
+# version: 0.0.292
 # date: 5 December 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-selfish-seo-layout-changes
@@ -23,10 +23,11 @@ after_initialize do
     private
 
     def strip_newlines_and_more_from_meta_description
-      Discourse.warn(">>>>NEONEO>>>>> @description_meta; #{@description_meta}", uri: request.env["REQUEST_URI"])
+      Discourse.warn(">>>>NEONEO_BEGIN>>>>> @description_meta: #{@description_meta}", uri: request.env["REQUEST_URI"])
       @description_meta&.gsub("&amp;hellip;", "")
       @description_meta&.gsub(/\n+/, " ")
       @description_meta&.gsub(/\s+/, " ")&.strip
+      Discourse.warn(">>>>NEONEO_END>>>>> @description_meta: #{@description_meta}", uri: request.env["REQUEST_URI"])
     end
   end
 end
