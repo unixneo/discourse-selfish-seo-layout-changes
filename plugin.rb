f994aac9cdb797053587ebe84db94b91565e7c52
neounix <neo@unix.com>
2025-02-21 17:01:07 +0700

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,8 +2,8 @@
 
 # name: discourse-selfish-seo-layout-changes
 # about: selfish plugin to remove canonical url, generator and change powered by, etc.
-# version: 0.0.9
-# date: 18 March 2021
+# version: 0.1.0
+# date: 21 Feb 2025
 # authors: Neo
 # url: https://github.com/unixneo/discourse-selfish-seo-layout-changes
 
